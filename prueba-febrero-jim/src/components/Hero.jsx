351afa90bf807c36f3ecbb6c28ea5aa4bd39e593
Juaigles <kiookaix@gmail.com>
2025-02-13 19:19:51 +0100

    style <hero> created hero section

--- prueba-febrero-jim/src/components/Hero.jsx
+++ prueba-febrero-jim/src/components/Hero.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='w-full h-[130px] bg-[#68B75E]'>
+    <div className='font-roboto w-full h-[130px] bg-[#68B75E] text-left  flex justify-left items-center pl-[71px]'>
+        <div>
 
+        <h2 className='py-[23px] font-bold text-[40px] leading-[46.88px]'>YOUR CARBON CREDITS MARKEPLACE <br /> <span className='text-[32px] leading-[37.5px]'>SUPPORT OUR PROJETCS. REDUCE YOUR CARBON FOOTPRINT.</span></h2>
+        </div>
     </div>
   )
 }
