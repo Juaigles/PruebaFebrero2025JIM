f1862fe01f3563d3b52f4e981155fc2f3efd1578
Juaigles <kiookaix@gmail.com>
2025-02-13 19:48:22 +0100

    final-prueba

--- prueba-febrero-jim/src/app/page.js
+++ prueba-febrero-jim/src/app/page.js
@@ -1,14 +1,20 @@
 import Buttons from "@/components/Buttons";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
-import Image from "next/image";
 
 export default function Home() {
   return (
-  <>
-  <Navbar/>
-  <Hero/>
-<Buttons/>
-  </>
+    <>
+      <Navbar />
+      <Hero />
+      
+      <div className="grid grid-cols-4 gap-4 p-4 justify-center items-center">
+        {Array.from({ length: 8 }).map((_, index) => (
+          <div key={index} className="">
+            <Buttons />
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
