d6e1cdfe2969f0b0ee5a010099c37f8ed9cb06cb
Juaigles <kiookaix@gmail.com>
2025-03-20 21:02:24 +0100

    style(buttonCards): Finished component style

--- prueba-febrero-jim/src/app/page.js
+++ prueba-febrero-jim/src/app/page.js
@@ -6,28 +6,79 @@ import SateliteButton from "@/components/SateliteButton";
 import SelectButtons from "@/components/SelectButtons";
 
 export default function Home() {
+  const data = [
+    {
+      img: ["Assets/imagenesTarjetas/image1.png","Assets/icons/biodiversity_stamp1.png"],
+      flag: "Assets/icons/italia.png",
+      tons: "195.122,74",
+      logos: ["Assets/icons/Vector.png", "Assets/icons/image24.png"],
+    },
+    {
+      img: ["Assets/imagenesTarjetas/image2.png"],
+      flag: "Assets/icons/italia.png",
+      tons: "884,74",
+      logos: ["Assets/icons/image21.png"],
+    },
+    {
+      img: ["Assets/imagenesTarjetas/image3.png"],
+      flag: "Assets/icons/greece.png",
+      tons: "562,05",
+      logos: ["Assets/icons/Vector.png", "Assets/icons/image18.png"],
+    },
+    {
+      img: ["Assets/imagenesTarjetas/image4.png"],
+      flag: "Assets/icons/germany.png",
+      tons: "230.122,99",
+      logos: ["Assets/icons/Vector.png", "Assets/icons/image21.png"],
+    },
+    {
+      img:  ["Assets/imagenesTarjetas/image5.png","Assets/icons/biodiversity_stamp1.png"],
+      flag: "Assets/icons/denmark.png",
+      tons: "5.122,11",
+      logos: [],
+    },
+    {
+      img:[ "Assets/imagenesTarjetas/image6.png"],
+      flag: "Assets/icons/italia.png",
+      tons: "80,74",
+      logos: ["Assets/icons/image24.png"],
+    },
+    {
+      img: ["Assets/imagenesTarjetas/image7.png"],
+      flag: "Assets/icons/belgium.png",
+      tons: "336.005",
+      logos: [],
+    },
+    {
+      img:  ["Assets/imagenesTarjetas/image8.png","Assets/icons/biodiversity_stamp1.png"],
+      flag: "Assets/icons/italia.png",
+      tons: "55.014,50",
+      logos: [],
+    },
+  ];
+
   return (
     <>
       <Navbar />
       <Hero />
       <div className="pl-[71px] pr-[87px] flex flex-col gap-[30px]">
         <div className="flex justify-between  pt-[20px]">
-   
-            <SateliteButton />
-
-
-            <NewProjectButton />
-
+          <SateliteButton />
+          <NewProjectButton />
         </div>
         <div>
-          <SelectButtons/>
+          <SelectButtons />
         </div>
 
-        <div className=" wrapper ">
+        <div className=" wrapper pb-[51px] ">
           {Array.from({ length: 8 }).map((_, index) => (
-            <div key={index} className="">
-              <Buttons img={"Assets/imagenesTarjetas/image1.png"} flag={"Assets/icons/italia.png"} tons={"195.122,74"}  logos={["Assets/icons/Vector.png", "Assets/icons/image24.png"]} />
-            </div>
+
+              <Buttons key={index}
+                img={data[index].img}
+                flag={data[index].flag}
+                tons={data[index].tons}
+                logos={data[index].logos}
+              />
           ))}
         </div>
       </div>
--- prueba-febrero-jim/src/components/Buttons.jsx
+++ prueba-febrero-jim/src/components/Buttons.jsx
@@ -1,32 +1,40 @@
 import React from 'react'
 
-const Buttons = ({ img, flag, tons, logos}) => {
+const Buttons = ({ img, flag, tons, logos }) => {
     return (
-        <div className='font-roboto w-[268px] h-[295px] border border-[#6C757D] flex flex-col text-black'>
-            <div className='w-[268px] h-[110px] border'>
-                <img src={img? img: null} alt="IMG" className=''/>
+        <div className='font-roboto w-[269px] h-[295px] border border-[#6C757D] rounded-md flex flex-col text-black'>
+            <div className='relative '>
+                {
+                    img[1] ?
+                        <img className='absolute  right-2 bottom-2 w-[34px] h-[29px]' src={img[1]} /> : null
+                }
+                <img src={img ? img[0] : null} alt="IMG" className='' />
             </div>
-            <div className='px-[12px] '>
-                <div className='flex justify-between'>
+            <div className='flex flex-col gap-2 px-[10px] py-[6px] '>
+                <div className='flex justify-between text-[12px] '>
 
-                    <div className='flex gap-[10px]  h-6'>
-                        <div className=' flex h-6 items-center'>
+                    <div className='flex gap-[10px] h-6'>
+                        <div className='flex items-center gap-2 '>
 
-                        <img src={flag} alt="" className='h-3 w-4'/>
+                            <img src={flag} alt="" className='h-3 w-4' />
+                            <p>2021</p>
                         </div>
-                        <p>2021</p>
                     </div>
-                    <div>
+                    <div className=' flex flex-col  text-right'>
                         <p>Available Tons <br />{tons}</p>
                     </div>
                 </div>
                 <div className='flex flex-col gap-[8px]'>
                     <a className='font-bold text-[#296480] text-[16px] leading-[14.63px]'>Project name</a>
-                    <p className='w-[233px] h-[78px] text-[12px] leading-[15px]'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae earum alias autem nulla quaerat! Quod rerum voluptatum facere perferendis </p>
+                    <p className=' text-[12px] leading-[15px]'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae earum alias autem nulla quaerat! Quod rerum voluptatum facere perferendis </p>
                 </div>
                 <hr />
-                <div>
-                    <img src={logos[0]} alt="" />
+                <div className='flex items-center  gap-1'>
+                    {logos.map((logo) => (
+
+                        <img src={logo} alt="" />
+
+                    ))}
                 </div>
             </div>
         </div>
